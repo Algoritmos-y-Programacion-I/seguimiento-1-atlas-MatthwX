23afec195b8f5cb773641edccbdd12d9580a60d3
Sara Patino Espitia <1109922056@u.icesi.edu.co>
2025-02-21 12:58:47 -0500

    Entrega

--- src/ui/Atlas.java
+++ src/ui/Atlas.java
@@ -16,6 +16,19 @@ public class Atlas {
 
     // Declaracion de constantes (completar)
     final double COSTO_TRAYECTO_AVION = 250000;
+    final double COSTO_TRAYECTO_BUS = 80000;
+    final double COSTO_NOCHES = 150000;
+    final int nombre;
+    final int edad;
+    final int tarjetaidentidad;
+    final int cedula;
+    final int ciudad;
+    final int noches;
+    final int transporte;
+    final int bus;
+    final int avion;
+    final int total;
+}
 
 	private Atlas() {
 		escaner = new Scanner(System.in);
@@ -36,29 +49,67 @@ public class Atlas {
         System.out.println("Bienvenido " + nombre + "!");
         // ...
 
-        // Declaracion de salidas calculadas (completar)
-        double totalTransporte;
-        // Calculo de salidas mediante metodos (completar)
-        totalTransporte = calcularTotalTransporte(); // aqui podrian faltar parametros
-	}
+        System.out.println("Cual es tu edad?");
+        edad = escaner.nextLine();
 
+        evaluarEdad(edad);
+
+        System.out.println("¿Desde donde te comunicas con nosotros?");
+        ciudad = escaner.nextLine();
+
+        System.out.println("Bienvenido a EVENTO ATLAS, ¿cuantas noches deseas hospedarte?");
+        noches = escaner.nextLine();
+
+/*
+        System.out.println("¿Que medio de transporte usaras? Selecciona 1 para avion o 2 para Bus");
+
+        evaluarTransporte(transporte);
+        */
+
+        int multiplicacion;
+        int COSTO_NOCHES = noches * COSTO_NOCHES;
+
+        
+    }
+	
+/*
 	public static void main(String[] args) {
 		Atlas mainApp = new Atlas();
 		mainApp.run();
 	}
+    */
 
     // Completar metodos para calcular salidas
 
+/*
     /**
 	 * Descripción: El método ... permite calcular ...
 	 * @param 
 	 * @return 
 	 */
+    /*
     public double calcularTotalTransporte() {
         return 0; // Completar operacion
+        */
+{
+        double totalTransporte
+        total = total + (total * 0.1);
+        System.out.println("Super! La vas a pasar genial, te quedaras " + noches " en Bogota. Este es el resumen de tu compra");
     }
 
-	
+    public static void evaluarEdad(int edad) {
+        if (edad > 18) {
+                System.out.println("Digite su cedula");
+            } else {
+                System.out.println("Digite su tarjeta de identidad");
 
 	
 }
+    }
+/*
+    public static void evaluarTransporte(int transporte) {
+        if (bus = 2) {
+                if (avion = 1);	
+}
+    }
+    */
